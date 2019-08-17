79218a92ad9019faa2e80c731dd13074e49d2379
hemantsf <https://github.com/Hemantsf/Leave-Task-Management-System>
2019-08-17 17:22:44 +0530

    Implement form validation and store the data

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route :: resource('questions','QuestionsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
