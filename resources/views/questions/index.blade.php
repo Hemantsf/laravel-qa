d815b532c38c4b17c46a21dc87c74cb1ebea412b
hemantsf <https://github.com/Hemantsf/Leave-Task-Management-System>
2019-08-17 12:02:14 +0530

    Add votes answers and views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,18 @@
                 <div class="card-body">
                 @foreach($questions as  $row)
                 <div class="media">
+                <div class="d-flex flex-column counters">
+                <div class="vote">
+                <strong> {{  $row->votes }}</strong> {{ str_plural ('vote',  $row->votes) }}
+                </div>
+                <div class="status {{  $row->status }}">
+                <strong> {{  $row->answers }}</strong> {{ str_plural ('answer',  $row->answers) }}
+                </div>
+                <div class="view">
+                {{  $row->views ." " . str_plural ('view',  $row->views) }}
+                </div>
+                
+                </div>
                 <div class="media-body">
                 <h3 class="mt-0"><a href ="{{ $row->url }}">{{ $row->title }}</h3></a>
                 <p class="lead">
